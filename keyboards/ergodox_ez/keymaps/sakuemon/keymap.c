11894171b43d40062bc5ec14670f60198a23e6f1
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2018-09-09 22:24:56 +0900

    change left bottom keys to arrows

--- keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
+++ keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(VIM),     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,          KC_BSLS,
                           KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,       KC_QUOT,
              TG(VIM),     KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,       KC_RSFT,
-                                  KC_RBRC, KC_NO  ,KC_NO ,KC_NO,          KC_NO,
+                                  KC_RBRC, KC_LEFT  ,KC_DOWN ,KC_UP,      KC_RGHT,
              KC_ESC, KC_LALT,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
