c280e334f8edc42e0c21e89b5e447df6876d40cb
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2018-09-09 22:12:28 +0900

    feel good

--- keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
+++ keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
@@ -4,7 +4,7 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
+#define VIM  1 // symbols
 #define MDIA 2 // media keys
 
 enum custom_keycodes {
@@ -23,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |   [  |                                       |   ]  |      |      |      |    |
+ *   | ESC  | ESC  |      |  `   |   [  |                                       |   ]  |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | Cmd  | LGui |       |Esc| LAlt |
+ *                                        | Cmd  | LCtrl|       | Esc  | LAlt |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 |Backsp|Delete|------|       |------|  Enter |Space |
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_NO,
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   TG(SYMB),
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   TG(VIM),
         KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_NO,
-        KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_LBRC,
+        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TG(VIM),
+        KC_ESC,         KC_ESC,  KC_NO,   KC_GRV,   KC_LBRC,
                                                KC_LGUI,  KC_LCTL,
                                                               KC_HOME,
                                                KC_BSPC,KC_DEL,KC_END,
         // right hand
              KC_NO,       KC_6,   KC_7,    KC_8,   KC_9,   KC_0,          KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,          KC_BSLS,
+             TG(VIM),     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,          KC_BSLS,
                           KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,       KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,       KC_RSFT,
+             TG(VIM),     KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,       KC_RSFT,
                                   KC_RBRC, KC_NO  ,KC_NO ,KC_NO,          KC_NO,
              KC_ESC, KC_LALT,
              KC_PGUP,
@@ -63,42 +63,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      | WORD |      |      |      |      |           |      |      |      |      |      |      |   F12  |
+ * |         |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      | FOWD |      |------|           |------| Right| Down |  Up  | Left |      |        |
+ * |         |      |      |      | FOWD |      |------|           |------| Left | Down |  Up  | RIght|      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |      |      |      |      | BKWD |      |           |      |      |      |      |      |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |      |      |      |      |                                       |      |      |      |      |      |
+ *   | EPRM  |      |      |      |      |                                       |      |      |      |      | RESET|
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |Animat|      |       |Toggle|Solid |
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |Bright|Bright|      |       |      |Hue-  |Hue+  |
- *                                 |ness- |ness+ |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// SYMBOLS
-[SYMB] = LAYOUT_ergodox(
+// VIM
+[VIM] = LAYOUT_ergodox(
        // left hand
        KC_NO,  KC_F1,  KC_F2,     KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_NO,  KC_NO,  S(KC_RGHT),KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
+       KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
        KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_PGDN,KC_NO,
        KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_NO,  KC_PGUP,KC_TRNS,
           EPRM,KC_TRNS,KC_TRNS,   KC_TRNS,KC_TRNS,
-                                       RGB_MOD,KC_TRNS,
+                                       KC_TRNS,KC_TRNS,
                                                KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+                               KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,KC_F11,
-       KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_F12,
-                KC_RGHT, KC_DOWN,KC_UP,   KC_LEFT, KC_NO, KC_TRNS,
        KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_TRNS,
-                         KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_TRNS,
-       RGB_TOG, RGB_SLD,
+                KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT, KC_NO, KC_TRNS,
+       KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_TRNS,
+                         KC_NO,  KC_NO,   KC_NO,   KC_NO, RESET,
+       KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(VIM)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
