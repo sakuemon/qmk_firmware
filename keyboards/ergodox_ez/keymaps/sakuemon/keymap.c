39d6d2a68199e2811a56c45220ef6e11fbc5cf2a
Tsuyoshi MIYAMOTO <sakuemon@gmail.com>
2018-09-10 20:11:27 +0900

    a little fix

--- keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
+++ keyboards/ergodox_ez/keymaps/sakuemon/keymap.c
@@ -29,34 +29,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | ESC  | ESC  |      |  `   |   [  |                                       |   ]  |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | Cmd  | LCtrl|       | Esc  | LAlt |
+ *                                        | Cmd  | Home |       | Esc  |  LAlt  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      | End  |       | PgUp |        |      |
  *                                 |Backsp|Delete|------|       |------|  Enter |Space |
- *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 |      |      | Cmd  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_NO,
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   TG(VIM),
-        KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   TG(VIM),
-        KC_ESC,         KC_ESC,  KC_NO,   KC_GRV,   KC_LBRC,
-                                               KC_LGUI,  KC_LCTL,
-                                                              KC_HOME,
-                                               KC_BSPC,KC_DEL,KC_END,
+        KC_EQL,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5, TG(VIM),
+        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T, MO(VIM),
+        KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,    KC_G,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B, MO(VIM),
+        KC_ESC,  KC_ESC, KC_NO,  KC_GRV, KC_LBRC,
+                                         KC_LGUI, KC_HOME,
+                                                  KC_END,
+                                 KC_BSPC,KC_DEL,  KC_LGUI,
         // right hand
-             KC_NO,       KC_6,   KC_7,    KC_8,   KC_9,   KC_0,          KC_MINS,
-             TG(VIM),     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,          KC_BSLS,
-                          KC_H,   KC_J,    KC_K,   KC_L,   KC_SCLN,       KC_QUOT,
-             TG(VIM),     KC_N,   KC_M,    KC_COMM,KC_DOT, KC_SLSH,       KC_RSFT,
-                                  KC_RBRC, KC_LEFT  ,KC_DOWN ,KC_UP,      KC_RGHT,
-             KC_ESC, KC_LALT,
-             KC_PGUP,
-             KC_PGDN,KC_ENT, KC_SPC
+                 TG(VIM), KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+                 MO(VIM), KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                          KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+                 MO(VIM), KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                  KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+                 KC_PGUP, KC_LALT,
+                 KC_PGDN,
+                 KC_ESC,  KC_ENT, KC_SPC
     ),
 /* Keymap 1: Vim like layer
  *
@@ -82,23 +82,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // VIM
 [VIM] = LAYOUT_ergodox(
        // left hand
-       KC_NO,  KC_F1,  KC_F2,     KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_NO,  KC_NO,  KC_TRNS,
-       KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_PGDN,KC_NO,
-       KC_TRNS,KC_NO,  KC_NO,     KC_NO,  KC_NO,  KC_PGUP,KC_TRNS,
-          EPRM,KC_TRNS,KC_TRNS,   KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_NO,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
+       KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+       KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_PGDN, KC_NO,
+       KC_TRNS, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_TRNS,
+          EPRM, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,
+                                          KC_TRNS, KC_TRNS,
+                                                   KC_TRNS,
+                                 KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,KC_F11,
-       KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_TRNS,
-                KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT, KC_NO, KC_TRNS,
-       KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_TRNS,
-                         KC_NO,  KC_NO,   KC_NO,   KC_NO, RESET,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+                KC_TRNS, KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10, KC_F11,
+                KC_TRNS, KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
+                         KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_NO,  KC_TRNS,
+                KC_TRNS, KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_TRNS,
+                                  KC_NO,   KC_NO,  KC_NO,   KC_NO,  RESET,
+                KC_TRNS, KC_TRNS,
+                KC_TRNS,
+                KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
